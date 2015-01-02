27bf59e5aea582d2b8f01eb8963f462601402157
Saqib Butt <saqib.butt@globalnorthstar.com>
2015-01-02 15:37:32 +0500

    Chapter 4.

--- src/tdd/DollarTest.java
+++ src/tdd/DollarTest.java
@@ -9,11 +9,23 @@ public class DollarTest {
 	@Test
 	public void testMultiplication() {
 		Dollar five= new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+		
+		/*
+		Dollar five= new Dollar(5);
+		Dollar product= five.times(2);
+		assertEquals(new Dollar(10), product);
+		product= five.times(3);
+		assertEquals(new Dollar(15), product);
+		*/
+		/*
+		Dollar five= new Dollar(5);
 		Dollar product= five.times(2);
 		assertEquals(10, product.getAmount());
 		product= five.times(3);
 		assertEquals(15, product.getAmount());
-		
+		*/
 		/*
 		Dollar five= new Dollar(5);
 		five.times(2);
